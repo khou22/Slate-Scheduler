5a443e9e663505199809c16e9e62a9ade0464c0f
Kevin Hou <kevin.ch.hou@gmail.com>
2017-01-06 18:31:50 -0800

    Starting iOS Calendar Integration

---
+++ Calendar Event Template App/CalendarManager.swift
@@ -0,0 +1,16 @@
+//
+//  CalendarManager.swift
+//  Calendar Event Template App
+//
+//  Created by Kevin Hou on 1/6/17.
+//  Copyright © 2017 Kevin Hou. All rights reserved.
+//
+
+import Foundation
+
+class CalendarManager {
+    
+    var eventStore: EKEventStore = EKEventStore()
+    
+    
+}
