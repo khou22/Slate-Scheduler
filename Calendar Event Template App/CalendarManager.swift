14a9aded7edaaf0157390b595df631b23f371a76
Kevin Hou <kevin.ch.hou@gmail.com>
2017-01-06 18:40:27 -0800

    Calendar authorization request set up

--- Calendar Event Template App/CalendarManager.swift
+++ Calendar Event Template App/CalendarManager.swift
@@ -5,12 +5,23 @@
 //  Created by Kevin Hou on 1/6/17.
 //  Copyright © 2017 Kevin Hou. All rights reserved.
 //
+//  Handles all communication with iOS calendar
 
 import Foundation
+import EventKit
 
 class CalendarManager {
     
     var eventStore: EKEventStore = EKEventStore()
     
-    
+    // Request access to read and add calendar events
+    func requestAccess() {
+        self.eventStore.requestAccess(to: .event, completion: { (granted, error) in
+            if (error != nil) {
+                print("Error \(error)")
+            } else {
+                print("Granted access \(granted)")
+            }
+        })
+    }
 }
