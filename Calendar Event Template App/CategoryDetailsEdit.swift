f2cb10dc0829fd0ad5895cfb6da85fa829675805
Kevin Hou <kevin.ch.hou@gmail.com>
2017-01-21 20:21:29 -0500

    Major styling changes to category details edit screen

--- Calendar Event Template App/CategoryDetailsEdit.swift
+++ Calendar Event Template App/CategoryDetailsEdit.swift
@@ -43,6 +43,8 @@ class CategoryDetailsEdit: UIViewController, UITableViewDelegate, UITableViewDat
             // Update category data with new markov model
             DataManager.updateOneCategory(with: self.currentCategory, index: self.selectedIndex)
             
+            print("Updated")
+            
             // Refresh the table view on this page
             self.refreshData()
         }))
@@ -84,6 +86,8 @@ class CategoryDetailsEdit: UIViewController, UITableViewDelegate, UITableViewDat
         for (eventName, _) in self.currentCategory.eventNameFreq {
             self.eventKeys.append(eventName) // Add keys
         }
+        
+        print(self.eventKeys.count)
         
         // Refresh table view
         DispatchQueue.main.async {
