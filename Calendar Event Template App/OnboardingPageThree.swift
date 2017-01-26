c2e59a7b95533a9cf9d4bb60441af1616f107a73
Kevin Hou <kevin.ch.hou@gmail.com>
2017-01-26 17:47:15 -0500

    Three summary cards transform correctly

--- Calendar Event Template App/OnboardingPageThree.swift
+++ Calendar Event Template App/OnboardingPageThree.swift
@@ -23,13 +23,17 @@ class OnboardingPageThree: UIViewController {
     
     func setupSummaryCards() {
         // Copy and create summary cards from original
-        self.summaryCardBottom = UIImageView(frame: self.summaryCard.frame) // Bottom summary card
+        self.summaryCardBottom = UIImageView(frame: self.summaryCard.frame.applying(CGAffineTransform(translationX: -10.0, y: 34.0))) // Bottom summary card with transform
         self.summaryCardBottom.image = self.summaryCard.image // Add image
-        self.summaryCardBottom.transform = CGAffineTransform(rotationAngle: CGFloat(Angles.radians(10.0))) // Rotate left
+        let transformBottom: CGAffineTransform = CGAffineTransform(rotationAngle: CGFloat(Angles.radians(12.0))) // Rotate rights
+        transformBottom.translatedBy(x: -2.0, y: -10.0) // Shift left and down
+        self.summaryCardBottom.transform = transformBottom // Apply transformation
         
-        self.summaryCardTop = UIImageView(frame: self.summaryCard.frame) // Top summary card
+        self.summaryCardTop = UIImageView(frame: self.summaryCard.frame.applying(CGAffineTransform(translationX: 3.0, y: -45.0))) // Top summary card with transform
         self.summaryCardTop.image = self.summaryCard.image // Add image
-        self.summaryCardTop.transform = CGAffineTransform(rotationAngle: CGFloat(Angles.radians(-10.0))) // Rotate right
+        let transformTop: CGAffineTransform = CGAffineTransform(rotationAngle: CGFloat(Angles.radians(-12.0))) // Rotate left
+        transformTop.translatedBy(x: 2.0, y: 50.0) // Shift right and up
+        self.summaryCardTop.transform = transformTop // Apply transformation
         
         view.insertSubview(self.summaryCardBottom, belowSubview: self.summaryCard) // Insert below original
         view.insertSubview(self.summaryCardTop, aboveSubview: self.summaryCard) // Insert above original
