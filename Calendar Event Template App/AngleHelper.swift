544c45b85d192905f1f9074ac9dfa0d5657a3d96
Kevin Hou <kevin.ch.hou@gmail.com>
2017-01-26 17:22:02 -0500

    Three cards on third page of on boarding now rotate

---
+++ Calendar Event Template App/AngleHelper.swift
@@ -0,0 +1,24 @@
+//
+//  AngleHelper.swift
+//  Calendar Event Template App
+//
+//  Created by Kevin Hou on 1/26/17.
+//  Copyright © 2017 Kevin Hou. All rights reserved.
+//
+//  Converting float values between degrees and radians
+
+import Foundation
+
+struct Angles {
+    // Convert from degrees to radians
+    static let degrees: (_ radians: Float) -> Float = { radians in
+        let degrees: Float = (radians * 360.0) / Float(2.0 * M_PI)
+        return degrees
+    }
+    
+    // Convert from radians to degrees
+    static let radians: (_ degrees: Float) -> Float = { degrees in
+        let radians: Float = (degrees * Float(2.0 * M_PI)) / 360.0
+        return radians
+    }
+}
--- Calendar Event Template App/OnboardingPageThree.swift
+++ Calendar Event Template App/OnboardingPageThree.swift
@@ -10,9 +10,28 @@ import Foundation
 import UIKit
 
 class OnboardingPageThree: UIViewController {
+    
+    // UI Elements
+    @IBOutlet weak var summaryCard: UIImageView! // Main event summary card image
+    var summaryCardBottom: UIImageView = UIImageView() // Bottom summary card (rotated right)
+    var summaryCardTop: UIImageView = UIImageView() // Top summary card (rotated left)
+    
     override func viewDidLoad() {
-        print("Onboarding page three loaded")
-        
         onboardingBackground() // Setup background gradient
+        setupSummaryCards() // Create the other two summary cards
+    }
+    
+    func setupSummaryCards() {
+        // Copy and create summary cards from original
+        self.summaryCardBottom = UIImageView(frame: self.summaryCard.frame) // Bottom summary card
+        self.summaryCardBottom.image = self.summaryCard.image // Add image
+        self.summaryCardBottom.transform = CGAffineTransform(rotationAngle: CGFloat(Angles.radians(10.0))) // Rotate left
+        
+        self.summaryCardTop = UIImageView(frame: self.summaryCard.frame) // Top summary card
+        self.summaryCardTop.image = self.summaryCard.image // Add image
+        self.summaryCardTop.transform = CGAffineTransform(rotationAngle: CGFloat(Angles.radians(-10.0))) // Rotate right
+        
+        view.insertSubview(self.summaryCardBottom, belowSubview: self.summaryCard) // Insert below original
+        view.insertSubview(self.summaryCardTop, aboveSubview: self.summaryCard) // Insert above original
     }
 }
