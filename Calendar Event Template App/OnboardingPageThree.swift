01d25826f7490bc23c9a2f898ea88efbebdd6cf2
Kevin Hou <kevin.ch.hou@gmail.com>
2017-01-29 00:17:59 -0500

    Added animations to onboarding page three

--- Calendar Event Template App/OnboardingPageThree.swift
+++ Calendar Event Template App/OnboardingPageThree.swift
@@ -10,8 +10,27 @@ import Foundation
 import UIKit
 
 class OnboardingPageThree: UIViewController {
+    
+    @IBOutlet weak var mapPinBottomConstraint: NSLayoutConstraint!
+    var originalMapPinBottomConstraintConst: CGFloat = -26.0
+    
     override func viewDidLoad() {
         onboardingBackground() // Setup background gradient
-        print("Onboarding page three loaded")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        self.entranceAnimations() // Start animations before screen appears
+    }
+    
+    func prepareEntranceAnimations() {
+        self.originalMapPinBottomConstraintConst = self.mapPinBottomConstraint.constant // Store
+    }
+    
+    func entranceAnimations() {
+        self.prepareEntranceAnimations() // Prepare animations
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 2.0, options: [.autoreverse, .repeat, .curveEaseIn], animations: {
+            self.mapPinBottomConstraint.constant = -12.0 // Move upwards
+            self.view.layoutIfNeeded()
+        }, completion: nil)
     }
 }
