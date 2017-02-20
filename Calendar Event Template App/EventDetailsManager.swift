3def4daec147a3973d706777277fa2136e12b527
Kevin Hou <kevin.ch.hou@gmail.com>
2017-02-19 19:45:08 -0500

    Fixed bug with text transparency

--- Calendar Event Template App/EventDetailsManager.swift
+++ Calendar Event Template App/EventDetailsManager.swift
@@ -280,13 +280,13 @@ extension EventDetails {
             // Animate out
             self.blackFade.layer.opacity = 0 // Transparent
             self.summaryCardTopConstraint.constant = self.summaryCardTopConstraint.constant - ScreenSize.screen_height // Move it off screen up
+            self.submitConfirmation.layer.opacity = 0.0 // Make label transparent
             self.view.layoutIfNeeded() // Animate update position
         }, completion: { finished in
-            // Adding event complete
+            // MARK - Adding event complete
             
             // Make transparent to prevent user from seeing it
             self.summaryCard.layer.opacity = 0.0
-            self.submitConfirmation.layer.opacity = 0.0
             
             // Exit segue back to category selection
             self.dismiss(animated: true, completion: nil)
