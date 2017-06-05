053325eaaafb8292dc6c846c3fb327fac1aa7366
Kevin Hou <kevin.ch.hou@gmail.com>
2017-06-05 08:10:20 +0800

    Cleaned up code

--- Calendar Event Template App/AutocompleteTextField.swift
+++ Calendar Event Template App/AutocompleteTextField.swift
@@ -232,79 +232,57 @@ extension AutocompleteTextField: UITableViewDelegate, UITableViewDataSource {
 import Foundation
 
 fileprivate extension String {
-    
     func getLevenshtein(target: String) -> Int {
         return levenshtein(sourceString: self, target: target)
     }
-    
 }
 
 // Minimize 3
 fileprivate func min3(a: Int, b: Int, c: Int) -> Int {
-    
     return min( min(a, c), min(b, c))
-    
 }
 
 fileprivate extension String {
     
     subscript(index: Int) -> Character {
-        
         return self[index]
-        
     }
     
     subscript(range: Range<Int>) -> String {
-        
         return self[range.lowerBound..<range.upperBound]
-        
     }
     
 }
 
 fileprivate struct Array2D {
-    
     var columns: Int
     var rows: Int
     var matrix: [Int]
     
-    
     init(columns: Int, rows: Int) {
-        
         self.columns = columns
-        
         self.rows = rows
         
         matrix = Array(repeating:0, count:columns*rows)
-        
     }
     
     subscript(column: Int, row: Int) -> Int {
-        
         get {
-            
             return matrix[columns * row + column]
-            
         }
         
         set {
-            
             matrix[columns * row + column] = newValue
-            
         }
         
     }
     
     func columnCount() -> Int {
-        
         return self.columns
-        
     }
     
     func rowCount() -> Int {
-        
         return self.rows
-        
     }
 }
 
@@ -318,37 +296,26 @@ fileprivate func levenshtein(sourceString: String, target targetString: String) 
     var distance = Array2D(columns: sourceLength + 1, rows: targetLength + 1)
     
     for x in 1...sourceLength {
-        
         distance[x, 0] = x
-        
     }
     
     for y in 1...targetLength {
-        
         distance[0, y] = y
-        
     }
     
     for x in 1...sourceLength {
-        
         for y in 1...targetLength {
-            
             if source[x - 1] == target[y - 1] {
-                
                 // no difference
                 distance[x, y] = distance[x - 1, y - 1]
-                
             } else {
-                
                 distance[x, y] = min3(
-                    
                     // deletions
                     a: distance[x - 1, y] + 1,
                     // insertions
                     b: distance[x, y - 1] + 1,
                     // substitutions
                     c: distance[x - 1, y - 1] + 1
-                    
                 )
                 
             }
@@ -356,7 +323,5 @@ fileprivate func levenshtein(sourceString: String, target targetString: String) 
         }
         
     }
-    
     return distance[source.count, target.count]
-    
 }
