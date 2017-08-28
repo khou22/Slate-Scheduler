0cb7a569df510f0a4725cd571a76a84649ab73f2
Kevin Hou <kevin.ch.hou@gmail.com>
2017-08-27 17:02:08 -0700

    Working day picker functionality on second page

--- Calendar Event Template App/EventDetailsMore.swift
+++ Calendar Event Template App/EventDetailsMore.swift
@@ -9,12 +9,24 @@
 import UIKit
 import EventKit
 
-class EventDetailsMore: UIViewController {
+class EventDetailsMore: UIViewController, UITableViewDelegate, UITableViewDataSource, DayPickerDelegate {
+    
+    @IBOutlet weak var dayPicker: DayPicker!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var eventListTable: UITableView!
+    var daysEvents: [EKEvent] = [] // Start empty
+    let calendarManager: CalendarManager = CalendarManager() // Instance of calendar manager
+    
     @IBOutlet weak var timeSlider: StepSlider!
     @IBOutlet weak var timeLabel: UILabel!
     
     @IBOutlet weak var durationSlider: StepSlider!
     @IBOutlet weak var durationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        dayPicker.delegate = self // Set delegate to self
+    }
 
     override func viewWillAppear(_ animated: Bool) {
         setInitialStates() // Sync with data struct
@@ -49,7 +61,6 @@ class EventDetailsMore: UIViewController {
     func updateTime() {
         var minutesFromMidnight = timeSlider.roundValue() * 3600.0 // Minutes from midnight
         data.event.time = minutesFromMidnight // Change global variable
-        print("Updated event time: \(data.event.time)")
         
         // Compensate for time zone
         minutesFromMidnight += -Double(NSTimeZone.local.secondsFromGMT()) // Apply time zone shift
@@ -63,5 +74,52 @@ class EventDetailsMore: UIViewController {
         durationLabel.text = self.durationSlider.roundString() + " hours" // Real time rounded value of slider
         data.event.duration = self.durationSlider.roundValue() * 3600.0 // Store round value in seconds
     }
+    
+    // MARK - Days Events Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.daysEvents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Cell for each index
+        let cell = eventListTable.dequeueReusableCell(withIdentifier: CellIdentifiers.eventListCell, for: indexPath) as! EventListCell
+        
+        let index = indexPath.item
+        cell.eventName.text = self.daysEvents[index].title
+        
+        // Time string
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "h:mm a" // Format
+        let startDate: Date = self.daysEvents[index].startDate
+        let endDate: Date = self.daysEvents[index].endDate
+        cell.eventTime.text = dateFormatter.string(from: startDate) + " - \n" + dateFormatter.string(from: endDate) // Concatinate string
+        
+        //        print(self.daysEvents[index].title)
+        
+        return cell
+    }
+    
+    func dateChange(date: Date) {
+        data.event.date = date // Update struct
+        updateDateLabel(newDate: date) // Update label
+        refreshDaysEvents()
+    }
+    
+    func refreshDaysEvents() {
+        daysEvents = calendarManager.getEvents(day: data.event.date.dateWithoutTime())
+        
+        // Refresh the table
+        DispatchQueue.main.async {
+            self.eventListTable.reloadData()
+        }
+    }
+    
+    func updateDateLabel(newDate: Date) {
+        // Change frontend label
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d" // Format
+        self.dateLabel.text = dateFormatter.string(from: newDate) + " at" // Update label
+    }
+
 }
 
